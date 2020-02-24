e6dbefcd1e1d1e8fef897c705389eca916c893c6
bgrovogel <56597159+bgrovogel@users.noreply.github.com>
2020-02-24 17:51:28 -0600

    Bank App

---
+++ app/src/main/java/com/example/dough/Register.java
@@ -0,0 +1,20 @@
+package com.example.dough;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void backbutton(View v) {
+        startActivity( new Intent( Register.this, MainActivity.class));
+    }
+}
